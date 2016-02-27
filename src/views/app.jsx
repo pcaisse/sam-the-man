ae28b5d491165e2ac5a015a96a486f4bb89cec8c
Peter Caisse <pcaisse@gmail.com>
2016-02-27 15:17:35 -0500

    Tabs to spaces

--- src/views/app.jsx
+++ src/views/app.jsx
@@ -3,25 +3,25 @@ var Level = require('./level.jsx');
 var MapData = require('../constants/mapData');
 
 var App = React.createClass({
-	getInitialState: function() {
-		return {
-			currLevel: 0
-		}
-	},
-	render: function() {
-		var map = MapData.map;
-		var styles = {
-			width: map.width,
-			height: map.height,
-			backgroundColor: '#e4e4e4'
-		};
-		var items = MapData.levels[this.state.currLevel].items;
-		return (
-			<div style={styles}>
-				<Level items={items} map={map} />
-			</div> 
-		);
-	}
+    getInitialState: function() {
+        return {
+            currLevel: 0
+        }
+    },
+    render: function() {
+        var map = MapData.map;
+        var styles = {
+            width: map.width,
+            height: map.height,
+            backgroundColor: '#e4e4e4'
+        };
+        var items = MapData.levels[this.state.currLevel].items;
+        return (
+            <div style={styles}>
+                <Level items={items} map={map} />
+            </div> 
+        );
+    }
 });
 
 module.exports = App;
--- src/views/level.jsx
+++ src/views/level.jsx
@@ -7,102 +7,112 @@ var models = require('../models');
 
 var Level = React.createClass({
 
-	getInitialState: function() {
-		return {
-			items: this.props.items
-		};
-	},
+    getInitialState: function() {
+        return {
+            items: this.props.items
+        };
+    },
 
-	componentDidMount: function() {
-		window.requestAnimationFrame(this.update);
-	},
+    componentDidMount: function() {
+        window.requestAnimationFrame(this.update);
+    },
 
-	update: function() {
-		var items = this.state.items;
-		items.forEach(this.updateItem);
-		this.setState(items);
-		window.requestAnimationFrame(this.update);
-	},
+    update: function() {
+        var items = this.state.items;
+        items.forEach(this.updateItem);
+        this.setState(items);
+        window.requestAnimationFrame(this.update);
+    },
 
-	updateItem: function(item, index, items) {
-    	if (item.canFall && !item.hasEntered && this.shouldContinueTo('fall', item, items)) {
-    		item.fall();
-    	} else if (item.canWalk && !item.hasEntered) {
-			if (!this.shouldContinueTo('walk', item, items)) {
-				var enteredEnterableItem = this.enteredEnterableItems(item, items)[0];
-				if (enteredEnterableItem) {
-					enteredEnterableItem.onEntered(item);
-				} else {
-					item.turn();
-				}
-			} else {
-		    	item.walk();
-			}
-    	} else if (item.canMoveVertically && !item.isStopped) {
-    		if (this.shouldContinueTo('moveVertically', item, items)) {
-				item.moveVertically();
-			} else {
-				item.onExited();
-			}
-    	}
-	},
+    updateItem: function(item, index, items) {
+        if (item.canFall && !item.hasEntered && !this.itemCollidesWithEnterableItem(item, items) &&
+                this.canContinueTo('fall', item, items)) {
+            item.fall();
+        } else if (item.canWalk && !item.hasEntered) {
+            if (!this.canContinueTo('walk', item, items)) {
+                var enteredEnterableItem = this.enteredEnterableItems(item, items)[0];
+                if (enteredEnterableItem) {
+                    enteredEnterableItem.onEntered(item);
+                } else {
+                    item.turn();
+                }
+            } else {
+                item.walk();
+            }
+        } else if (item.canMoveVertically && !item.isStopped) {
+            if (this.canContinueTo('moveVertically', item, items)) {
+                item.moveVertically();
+            } else {
+                item.onExited();
+            }
+        }
+    },
 
-	itemCollidesWithCollidableItem: function(item, items) {
-		return items.filter(function(currItem) {
-			return currItem.isCollidable && currItem !== item;
-		}).some(function(collidableItem) {
-			return item.collidesWith(collidableItem);
-		});
-	},
+    // TODO: Make these functions into methods on an Items collection
+    itemCollidesWithEnterableItem: function(item, items) {
+        return items.filter(function(currItem) {
+            return currItem.isEnterable && currItem !== item;
+        }).some(function(collidableItem) {
+            return item.collidesWith(collidableItem);
+        });
+    },
 
-	enteredEnterableItems: function(item, items) {
-		return items.filter(function(currItem) {
-			if (currItem.isEnterable && currItem !== item) {
-				return item.hasSamePosition(currItem);
-			}
-		});
-	},
+    itemCollidesWithCollidableItem: function(item, items) {
+        return items.filter(function(currItem) {
+            return currItem.isCollidable && currItem !== item;
+        }).some(function(collidableItem) {
+            return item.collidesWith(collidableItem);
+        });
+    },
+
+    enteredEnterableItems: function(item, items) {
+        return items.filter(function(currItem) {
+            if (currItem.isEnterable && currItem !== item) {
+                return item.hasSamePosition(currItem);
+            }
+        });
+    },
 
     itemIsWithinMapY: function(item) {
-    	return item.top >= 0 && item.top <= this.props.map.height - item.height;
+        return item.top >= 0 && item.top <= this.props.map.height - item.height;
     },
 
     itemIsWithinMapX: function(item) {
-		return item.left >= 0 && item.left <= this.props.map.width - item.width;
+        return item.left >= 0 && item.left <= this.props.map.width - item.width;
     },
 
     itemIsWithinMapBounds: function(item) {
-    	return this.itemIsWithinMapX(item) && this.itemIsWithinMapY(item);
+        return this.itemIsWithinMapX(item) && this.itemIsWithinMapY(item);
     },
 
-    shouldContinueTo: function(funcName, item, items) {
-		var futureItem = Object.assign({}, item);
-		futureItem[funcName].call(futureItem);
-		return this.itemIsWithinMapBounds(futureItem) && !this.itemCollidesWithCollidableItem(futureItem, items);
+    canContinueTo: function(funcName, item, items) {
+        var futureItem = Object.assign({}, item);
+        futureItem[funcName].call(futureItem);
+        return this.itemIsWithinMapBounds(futureItem) && !this.itemCollidesWithCollidableItem(futureItem, items);
     },
 
     modelsToComponents: function(model, Component) {
-    	return this.props.items.filter(function(item) {
-			return item instanceof model;
-		}).map(function(item, index) {
-			return <Component key={index} {...item} />;
-		});
+        return this.props.items.filter(function(item) {
+            return item instanceof model;
+        }).map(function(item, index) {
+            return <Component key={index} {...item} />;
+        });
     },
 
-	render: function() {
-		var styles = {
-			position: 'relative',
-			width: this.props.map.width,
-			height: this.props.map.height,
-		};
-		return (
-			<div style={styles}>
-				{this.modelsToComponents(models.Man, Man)}
-				{this.modelsToComponents(models.Block, Block)}
-				{this.modelsToComponents(models.Elevator, Elevator)}
-			</div> 
-		);
-	},
+    render: function() {
+        var styles = {
+            position: 'relative',
+            width: this.props.map.width,
+            height: this.props.map.height,
+        };
+        return (
+            <div style={styles}>
+                {this.modelsToComponents(models.Man, Man)}
+                {this.modelsToComponents(models.Block, Block)}
+                {this.modelsToComponents(models.Elevator, Elevator)}
+            </div> 
+        );
+    },
 });
 
 module.exports = Level;
